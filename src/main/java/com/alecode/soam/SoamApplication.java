11d186cc683db877196cacd1943f66653f541fcc
Alec Burgess <alec_burgess@rush.edu>
2022-11-30 20:09:37 -0600

    Refactor package name.
    change SOAM to lowercase

--- src/main/java/com/alecode/soam/SoamApplication.java
+++ src/main/java/com/alecode/soam/SoamApplication.java
@@ -1,4 +1,4 @@
-package com.alecode.SOAM;
+package com.alecode.soam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/alecode/soam/SoamApplicationTests.java
+++ src/test/java/com/alecode/soam/SoamApplicationTests.java
@@ -1,4 +1,4 @@
-package com.alecode.SOAM;
+package com.alecode.soam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
