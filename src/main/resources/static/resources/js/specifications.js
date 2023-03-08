0158a52a44b59b58f9fd8caf1aebd72148acd3c2
Alexandr Ufimtsev <aufimtsev@gmail.com>
2023-03-08 10:30:10 +0200

    cleanup in jstree loader main panel shows consistent message when no entity is selected new tree is loaded by default

--- src/main/resources/static/resources/js/specifications.js
+++ src/main/resources/static/resources/js/specifications.js
@@ -4,120 +4,115 @@ const TREE_ICON_MAP = {
     'stakeholder': 'bi bi-person-check-fill',
     'stakeholderObjective': 'bi bi-check-circle'
 };
+const DEFAULT_MESSAGE = 'Please select an item in Entity Explorer';
 
-$(document).ready(function () {
-    $('#tree').jstree({
-        'core': {
-            'data': (obj, callback) => {
-                if (obj.id === '#') {
-                    callback([{
-                        'text': 'Specifications',
-                        'data': {
-                            'type': 'specifications'
-                        },
-                        'children': true
-                    }]);
-                } else {
-                    switch (obj.data.type) {
-                        case 'specifications':
-                            fetch('/tree/specification')
-                                .then(response => response.json())
-                                .then(items => {
-                                    const children = items.map(item => {
-                                        return {
-                                            'text': item.name,
-                                            'icon': TREE_ICON_MAP[item.type],
-                                            'children': [
-                                                {
-                                                    'text': 'Objectives',
-                                                    'data': {
-                                                        'type': 'specificationObjectives',
-                                                        'specificationId': item.id
-                                                    },
-                                                    'children': true
-                                                },
-                                                {
-                                                    'text': 'Stakeholders',
-                                                    'data': {
-                                                        'type': 'stakeholders',
-                                                        'specificationId': item.id
-                                                    },
-                                                    'children': true
-                                                }
-                                            ],
-                                            'data': {
-                                                'type': item.type,
-                                                'id': item.id
-                                            }
-                                        };
-                                    });
-                                    callback(children)
-                                });
-                            break;
-                        case 'specificationObjectives':
-                            fetch('/tree/specification/' + obj.data.specificationId + '/specificationObjective')
-                                .then(response => response.json())
-                                .then(items => {
-                                    const children = items.map(item => {
-                                        return {
-                                            'text': item.name,
-                                            'icon': TREE_ICON_MAP[item.type],
-                                            'data': {
-                                                'type': item.type,
-                                                'id': item.id
-                                            }
-                                        };
-                                    });
-                                    callback(children)
-                                });
-                            break;
-                        case 'stakeholders':
-                            fetch('/tree/specification/' + obj.data.specificationId + '/stakeholder')
-                                .then(response => response.json())
-                                .then(items => {
-                                    const children = items.map(item => {
-                                        return {
-                                            'text': item.name,
-                                            'icon': TREE_ICON_MAP[item.type],
-                                            'children': [{
-                                                    'text': 'Stakeholder Objectives',
-                                                    'data': {
-                                                        'type': 'stakeholderObjectives',
-                                                        'stakeholderId': item.id
-                                                    },
-                                                    'children': true
-                                            }],
-                                            'data': {
-                                                'type': item.type,
-                                                'id': item.id
-                                            }
-                                        };
-                                    });
-                                    callback(children)
-                                });
-                            break;
-                        case 'stakeholderObjectives':
-                            fetch('/tree/stakeholder/' + obj.data.stakeholderId + '/stakeholderObjective')
-                                .then(response => response.json())
-                                .then(items => {
-                                    const children = items.map(item => {
-                                        return {
-                                            'text': item.name,
-                                            'icon': TREE_ICON_MAP[item.type],
-                                            'data': {
-                                                'type': item.type,
-                                                'id': item.id
-                                            }
-                                        };
-                                    });
-                                    callback(children)
-                                });
-                            break;
-                    }
-                }
-            }
+function createTreeEntity(item) {
+    return {
+        'text': item.name,
+        'icon': TREE_ICON_MAP[item.type],
+        'data': {
+            'type': item.type,
+            'id': item.id
+        }
+    };
+}
+
+function jsTreeDataLoader(obj, callback) {
+    if (obj.id === '#') {
+        callback([{
+            'text': 'Specifications',
+            'state': {
+                'opened': true
+            },
+            'data': {
+                'type': 'specifications'
+            },
+            'children': true
+        }]);
+    } else {
+        switch (obj.data.type) {
+            case 'specifications':
+                fetch('/tree/specification')
+                    .then(response => response.json())
+                    .then(items => {
+                        const children = items.map(item => {
+                            const treeEntity = createTreeEntity(item);
+                            treeEntity.children = [
+                                {
+                                    'text': 'Objectives',
+                                    'data': {
+                                        'type': 'specificationObjectives',
+                                        'specificationId': item.id
+                                    },
+                                    'children': true
+                                },
+                                {
+                                    'text': 'Stakeholders',
+                                    'data': {
+                                        'type': 'stakeholders',
+                                        'specificationId': item.id
+                                    },
+                                    'children': true
+                                }
+                            ];
+                            return treeEntity;
+                        });
+                        callback(children);
+                    });
+                break;
+            case 'specificationObjectives':
+                fetch('/tree/specification/' + obj.data.specificationId + '/specificationObjective')
+                    .then(response => response.json())
+                    .then(items => {
+                        const children = items.map(createTreeEntity);
+                        callback(children);
+                    });
+                break;
+            case 'stakeholders':
+                fetch('/tree/specification/' + obj.data.specificationId + '/stakeholder')
+                    .then(response => response.json())
+                    .then(items => {
+                        const children = items.map(item => {
+                            const treeEntity = createTreeEntity(item);
+                            treeEntity.children = [{
+                                'text': 'Stakeholder Objectives',
+                                'data': {
+                                    'type': 'stakeholderObjectives',
+                                    'stakeholderId': item.id
+                                },
+                                'children': true
+                            }];
+                            return treeEntity;
+                        });
+                        callback(children);
+                    });
+                break;
+            case 'stakeholderObjectives':
+                fetch('/tree/stakeholder/' + obj.data.stakeholderId + '/stakeholderObjective')
+                    .then(response => response.json())
+                    .then(items => {
+                        const children = items.map(createTreeEntity);
+                        callback(children);
+                    });
+                break;
         }
     }
+}
+
+function loadMainPanel(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(text => $('#main').html(text));
+}
+
+$(document).ready(function () {
+    loadMainPanel('/specifications2/default');
+
+    $('#tree').jstree({
+        'core': {
+            'data': jsTreeDataLoader
+        }
+    });
 
 
     /*{
@@ -135,21 +130,20 @@ $(document).ready(function () {
                 }
             }
         }
-    }*/);
+    });*/
     $('#tree').on('activate_node.jstree', (e, data) => {
         console.log("activate_node: data: " + data);
+        let url;
         if (data.node.data.id) {
-            let url = '/' + data.node.data.type + '2/' + data.node.data.id + '/edit';
+            url = '/' + data.node.data.type + '2/' + data.node.data.id + '/edit';
             for (let i = 1; i < data.node.parents.length - 1; i += 2) {
                 const parentData = $('#tree').jstree().get_node(data.node.parents[i]).data;
                 url = '/' + parentData.type + '/' + parentData.id + url;
             }
-            fetch(url)
-                .then(response => response.text())
-                .then(text => $('#main').html(text));
         } else {
-            $('#main').html('');
+            url = '/specifications2/default';
         }
+        loadMainPanel(url);
     });
 })
 
